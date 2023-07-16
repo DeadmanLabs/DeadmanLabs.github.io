95a4c1561f809eb56222ccaa4194913b14ce582d
DeadmanLabs <michael.magahey@gmail.com>
2023-07-16 01:17:49 -0400

    More blog updates

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -6,14 +6,48 @@ import { Outlet, Link } from 'react-router-dom';
 
 import './Styles/Blog.css';
 
-const importAll = (r) => r.keys().map(r);
-const posts = importAll(require.context('./Blogs', false, /\.md$/));
+const BlogItem = (props) => {
+    const [loading, setLoading] = useState(false);
+    const [image, setImage] = useState(null);
+
+    useEffect(() => {
+        let active = true;
+        load();
+        return () => { active  = false }
+
+        async function load() {
+            setLoading(true);
+            const response = fetch('https://localhost:3000/blog/image',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    id: props.id
+                })
+            });
+            if (!active) { return; }
+            setLoading(false);
+        }
+    }, []);
+
+    return (
+        <div className="blog-post" href={props.url}>
+            <img src={ loading || image === null ? "../../public/Resources/Status/Loading.gif" : image} />
+            <a>{props.name}</a>
+            <a>{props.author}</a>
+            <a>{props.date}</a>
+        </div>
+    );
+}
 
 const Blog = (props) => {
     const { connection } = useConnection();
     const { publicKey, sendTransaction } = useWallet();
 
     const [loading, setLoading] = useState(false);
+    const [posts, setPosts] = useState([]);
 
     useEffect(() => {
         let active = true;
@@ -22,16 +56,22 @@ const Blog = (props) => {
 
         async function load() {
             setLoading(true);
-            //...
+            fetch('https://localhost:3000/blog/posts')
+                .then(response => response.json())
+                .then(data => setPosts(data.posts));
             if (!active) { return; }
             //...
             setLoading(false);
         }
-    }, [connection, publicKey]);
+    }, []);
 
     return (
         <div className="blog">
-
+            {
+                posts.map((value) => {
+                    <BlogItem id={value.id} name={value.name} author={value.author} date={value.date} url={value.url} />
+                })
+            }
         </div>
     );
 }
